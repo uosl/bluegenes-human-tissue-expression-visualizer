393f071a5ccc7c3432bb3ce948b8c2efa09a4724
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2020-06-16 17:05:40 +0530

    added loading

--- src/RootContainer.js
+++ src/RootContainer.js
@@ -3,18 +3,31 @@ import { queryData } from './query';
 
 const RootContainer = ({ serviceUrl, entity }) => {
 	const [data, setData] = useState([]);
+	const [loading, setLoading] = useState(false);
+
 	useEffect(() => {
+		setLoading(true);
 		let { value } = entity;
 		queryData({
 			serviceUrl: serviceUrl,
 			geneId: !Array.isArray(value) ? [value] : value
 		}).then(data => {
 			setData(data);
+			setLoading(false);
 		});
 	}, []);
+
 	return (
 		<div className="rootContainer">
-			<h1>Your Data Viz Here</h1>
+			{loading ? (
+				<h1>Loading...</h1>
+			) : (
+				<div className="innerContainer">
+					<div className="graph">
+						<span className="chart-title">Gene Tissue Network</span>
+					</div>
+				</div>
+			)}
 		</div>
 	);
 };
